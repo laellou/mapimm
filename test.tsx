2164f39ec5bdd596b77ff0de2c6a7f90731dba6c
laellou ferdinand <laellou@hotmail.fr>
2021-11-09 17:59:09 +0100

    my map

---
+++ test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="32" tileheight="32" tilecount="112" columns="8">
+ <image source="../../Downloads/TileSet_V1.png" width="256" height="448"/>
+</tileset>
